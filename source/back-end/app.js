94c9b329ab6937821e75a140da345a1871d3430e
blas <blashernandez98@gmail.com>
2023-08-16 13:16:13 -0300

    Update default port

--- source/back-end/app.js
+++ source/back-end/app.js
@@ -33,7 +33,7 @@ async function main() {
   })
 
   // Start the server
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 8080
   app.listen(port, () => {
     console.log(`Server started on port ${port}`)
   })
