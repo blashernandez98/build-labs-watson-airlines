96831577fc7a5d818f4159d17fd29a527fb60fa7
blas <blashernandez98@gmail.com>
2023-08-20 23:13:07 -0300

    Basic style and content

--- source/front-end/src/App.jsx
+++ source/front-end/src/App.jsx
@@ -1,5 +1,16 @@
+import Header from './Components/Header'
+import './App.css'
+import FaqSection from './Components/Faq'
+import Footer from './Components/Footer'
+
 function App() {
-  return <div className='text-2xl font-bold'>Watson Airlines</div>
+  return (
+    <div className=''>
+      <Header />
+      <FaqSection />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ source/front-end/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='flex flex-row justify-between items-center bg-zinc-800 px-20 py-10'>
+      <h3 className='text-slate-50'>Watson Airlines Inc.</h3>
+    </footer>
+  )
+}
+
+export default Footer
